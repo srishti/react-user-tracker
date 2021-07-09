9c13fd0dbf646241c5a57e51ff6efa5a0fb31012
Srishti Gupta <mail@srishtigupta.in>
2021-07-09 11:37:31 +0530

    added portals for backdrop and modal overlay

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,37 +1,54 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import Card from "./Card";
 import Button from "./Button";
 
 import styles from "./Modal.module.css";
 
+const Backdrop = (props) => {
+  console.log("[Backdrop] rendered");
+
+  return <div className={styles.backdrop} onClick={props.onClose}></div>;
+};
+
+const ModalOverlay = (props) => {
+  console.log("[ModalOverlay] rendered");
+
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button type="button" onClick={props.onClose} className={styles.button}>
+          Okay
+        </Button>
+      </footer>
+    </Card>
+  );
+};
+
 const Modal = (props) => {
   console.log("[Modal] rendered");
 
-  const closeModalHandler = () => {
-    props.onReset();
-  };
-
   return (
     <>
-      <div className={styles.backdrop} onClick={closeModalHandler}></div>
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button
-            type="button"
-            onClick={closeModalHandler}
-            className={styles.button}
-          >
-            Okay
-          </Button>
-        </footer>
-      </Card>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onReset} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onClose={props.onReset}
+        />,
+        document.getElementById("overlay-root")
+      )}
     </>
   );
 };
